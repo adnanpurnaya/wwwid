c169951e9108461b64e814ef443a5e164f81ecf5
Adnan Purnaya <adnanpurnaya@gmail.com>
2018-03-07 14:41:37 +0700

    =fix style

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-importScripts('/wwwid/_nuxt/workbox.3de3418b.js')
+importScripts('/_nuxt/workbox.3de3418b.js')
 
 const workboxSW = new self.WorkboxSW({
   "cacheId": "wwwid",
@@ -8,41 +8,37 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/wwwid/_nuxt/app.f9976152508111edf6f0.js",
-    "revision": "fa21c9526a1be2e610d04bcabbfeb804"
+    "url": "/_nuxt/app.421fc72045b97250dd9b.js",
+    "revision": "e452f9a305d6e1bb5f679e37f88504b5"
   },
   {
-    "url": "/wwwid/_nuxt/layouts/default.1f81aee4dfbd8fe4f9ea.js",
-    "revision": "54332c225fbf96d1de740799563c116c"
+    "url": "/_nuxt/layouts/default.981efef4ca56531c8424.js",
+    "revision": "5e9ea20a89e0432863a42549fd1092e7"
   },
   {
-    "url": "/wwwid/_nuxt/manifest.d3feb4b12a8d1cdd2058.js",
-    "revision": "f277c3ad8c38200c660fb04a25262114"
+    "url": "/_nuxt/manifest.51275df5cf4b7be92b26.js",
+    "revision": "422f336b00b2bb9006815bbb5339f33c"
   },
   {
-    "url": "/wwwid/_nuxt/pages/_slug/index.bb5260c44cd75ec80397.js",
+    "url": "/_nuxt/pages/_slug/index.bb5260c44cd75ec80397.js",
     "revision": "9f6593dcba055092ac505c162ac93811"
   },
   {
-    "url": "/wwwid/_nuxt/pages/about/index.14dd40437b3d6b14d37d.js",
-    "revision": "e6b96f1116fce6a6836d3ff570cfb0f7"
+    "url": "/_nuxt/pages/index.8d1e853a334fbcaa4c64.js",
+    "revision": "f9ec83f666b811b791510ba5743866f1"
   },
   {
-    "url": "/wwwid/_nuxt/pages/index.e4790389c308d3772516.js",
-    "revision": "5e56d91bfd8629a6d5a9648fb620e398"
+    "url": "/_nuxt/pages/kategori/_name/index.9a3c1b6e3d32c85b57e8.js",
+    "revision": "25ae839f03a749ccc617ed67032c055d"
   },
   {
-    "url": "/wwwid/_nuxt/pages/kategori/_name/index.eddcc76027a89509568b.js",
-    "revision": "c485fd5aa5c7c2febdc980239b431b75"
-  },
-  {
-    "url": "/wwwid/_nuxt/vendor.7a3ece053cdc7dbd1c6c.js",
-    "revision": "e2e420e35ab809400844f90f5c10b4fe"
+    "url": "/_nuxt/vendor.0d4fbe687f4678897cb8.js",
+    "revision": "b123e8718878ae94aaa6d137eff2db03"
   }
 ])
 
 
-workboxSW.router.registerRoute(new RegExp('/wwwid/_nuxt/.*'), workboxSW.strategies.cacheFirst({}), 'GET')
+workboxSW.router.registerRoute(new RegExp('/_nuxt/.*'), workboxSW.strategies.cacheFirst({}), 'GET')
 
-workboxSW.router.registerRoute(new RegExp('/wwwid/.*'), workboxSW.strategies.networkFirst({}), 'GET')
+workboxSW.router.registerRoute(new RegExp('/.*'), workboxSW.strategies.networkFirst({}), 'GET')
 
